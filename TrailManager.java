94f936d1f6b4b78cb03b176527ba6513b875a3d9
CannotLoadName <electricfan@yeah.net>
2024-05-24 22:04:18 +0800

    项目文件

---
+++ TrailManager.java
@@ -0,0 +1,67 @@
+package biomesoplenty.common.remote;
+
+import java.util.HashMap;
+import java.util.UUID;
+
+public class TrailManager {
+    private static final String REMOTE_TRAILS_FILE = "https://raw.githubusercontent.com/Glitchfiend/BiomesOPlenty/master/trails.txt";
+    public static HashMap trailsMap = new HashMap();
+    public static HashMap trailTextures = new HashMap();
+
+    public static void retrieveTrails() {
+        trailsMap.put(UUID.fromString("e358f277-4a4c-42f6-9ad8-3addf4869ea6"), "dev_trail");
+        trailsMap.put(UUID.fromString("6bdd4acd-5637-4488-9858-3de07cc820d5"), "dev_trail");
+        trailsMap.put(UUID.fromString("6ac7c57d-8c15-4ffe-ae5d-5e04bd606786"), "dev_trail");
+        trailsMap.put(UUID.fromString("748a5f29-f242-42c7-b7d9-652f2dd64a4e"), "dev_trail");
+        trailsMap.put(UUID.fromString("22266381-3822-4924-8dc9-dd20d4b9fd4d"), "donator_trail");
+        trailsMap.put(UUID.fromString("eb21559e-bb22-46f2-897b-71eee2d5c09b"), "donator_trail");
+        trailsMap.put(UUID.fromString("69f112ef-ed1d-4185-9cbd-14f6954d6b14"), "donator_trail");
+        trailsMap.put(UUID.fromString("55531f99-b671-4b3c-91c4-667fab95b622"), "donator_trail");
+        trailsMap.put(UUID.fromString("e4c79a56-ac81-467b-850e-ed6e1e77eb40"), "donator_trail");
+        trailsMap.put(UUID.fromString("12cad963-8dca-4540-b002-07f3039cc5a7"), "donator_trail");
+        trailsMap.put(UUID.fromString("8e1a465d-b26e-4274-aadf-47bfbd49f35a"), "donator_trail");
+        trailsMap.put(UUID.fromString("24630ce4-ff70-4dff-867b-ba0e71b48940"), "donator_trail");
+        trailsMap.put(UUID.fromString("8cf61715-6216-4903-bb03-e9653c4eba15"), "donator_trail");
+        trailsMap.put(UUID.fromString("ccebe60d-b649-4ef5-82d6-7d12f9671b61"), "donator_trail");
+        trailsMap.put(UUID.fromString("197bef44-e979-46d6-bdfd-c3a6f645ffe4"), "donator_trail");
+        trailsMap.put(UUID.fromString("57cc09c4-0440-4eef-9419-8aa4425cfe12"), "donator_trail");
+        trailsMap.put(UUID.fromString("9926753a-434e-4b46-a076-25993a07b8ef"), "donator_trail");
+        trailsMap.put(UUID.fromString("d859c5ea-37e9-43d7-b3b9-523e448bfda0"), "donator_trail");
+        trailsMap.put(UUID.fromString("d1ab8e20-2e07-499b-b590-b4b12c1fbb42"), "donator_trail");
+        trailsMap.put(UUID.fromString("06d348b6-9416-4feb-8e82-1eb541502471"), "donator_trail");
+        trailsMap.put(UUID.fromString("5cd25fad-2597-4aa2-a3ff-24272e17d739"), "donator_trail");
+        trailsMap.put(UUID.fromString("208cbb56-cb77-498b-9667-a8bb9dcdec2e"), "donator_trail");
+        trailsMap.put(UUID.fromString("a6ddf0be-b1dd-4e40-b3dc-0437df8518cf"), "donator_trail");
+        trailsMap.put(UUID.fromString("c7b62bb0-8a7a-41f8-a9d9-812c89ee9c99"), "donator_trail");
+        trailsMap.put(UUID.fromString("bc742c22-7842-48f6-9349-5379865d962e"), "donator_trail");
+        trailsMap.put(UUID.fromString("deec0b03-d84d-47df-bce4-ebfd0fe6ab80"), "donator_trail");
+        trailsMap.put(UUID.fromString("c44427b3-fd3c-484e-a6cf-04b2ad92ef27"), "donator_trail");
+        trailsMap.put(UUID.fromString("b9726d8f-0455-427f-87f2-a24adfcd7268"), "donator_trail");
+        trailsMap.put(UUID.fromString("976f3e28-90f3-4e46-b7be-23ff2979ab0e"), "donator_trail");
+        trailsMap.put(UUID.fromString("a1dd242b-1cc0-43a6-8003-46e062e79525"), "donator_trail");
+        trailsMap.put(UUID.fromString("61fa5245-82a0-4985-8644-5d2f775ce1d5"), "donator_trail");
+        trailsMap.put(UUID.fromString("bef0a130-c4f5-4239-bd6b-a19e24802120"), "donator_trail");
+        trailsMap.put(UUID.fromString("ff144975-7b5a-4494-aa4f-55a07db35e18"), "donator_trail");
+        trailsMap.put(UUID.fromString("9f070456-0f8a-47a3-abf2-6ac389686b93"), "donator_trail");
+        trailsMap.put(UUID.fromString("6a419733-b550-46d3-a21c-46656b801849"), "donator_trail");
+        trailsMap.put(UUID.fromString("83a00245-b186-4cda-a11d-c0c5fff4da1f"), "donator_trail");
+        trailsMap.put(UUID.fromString("e6b11b56-eff5-4067-9ba5-1f670b68177d"), "donator_trail");
+        trailsMap.put(UUID.fromString("e1c9f4ca-fa2b-428e-bd04-b237210a95f6"), "donator_trail");
+        trailsMap.put(UUID.fromString("0e68c5f4-cf53-4afe-9736-6ae226423090"), "donator_trail");
+        trailsMap.put(UUID.fromString("30f09699-9b4f-4192-aae1-7bc41c722853"), "donator_trail");
+        trailsMap.put(UUID.fromString("3a17faaa-e13a-4dc0-8c49-be5bb5c59dbf"), "donator_trail");
+        trailsMap.put(UUID.fromString("fc363256-d8bd-44cd-a316-7aaf399ea9b8"), "donator_trail");
+        trailsMap.put(UUID.fromString("6a905523-534d-42a6-a805-b9afbefc59a3"), "donator_trail");
+        trailsMap.put(UUID.fromString("196826a5-9e07-4d7f-9ac2-53100048a454"), "donator_trail");
+        trailsMap.put(UUID.fromString("25a9a487-a415-4f14-a3ce-ca58b819c693"), "donator_trail");
+        trailsMap.put(UUID.fromString("7c53c7e1-efb8-4ab2-a762-cd7d05d6e79b"), "donator_trail");
+        trailsMap.put(UUID.fromString("ec887a47-b50b-492c-b675-3e8e93a0428b"), "donator_trail");
+        trailsMap.put(UUID.fromString("f3ba6ec8-c280-4950-bb08-1fcb2eab3a9c"), "helper_trail");
+        trailsMap.put(UUID.fromString("20edfd61-1e2f-4709-b4a9-7e92dbb994ce"), "helper_trail");
+        trailsMap.put(UUID.fromString("794cf289-670e-4951-b630-45806e9fa8fe"), "helper_trail");
+        trailsMap.put(UUID.fromString("3c1dc8d0-cee8-42dd-a48d-4056f7635fac"), "helper_trail");
+        trailTextures.put("dev_trail", (Object) null);
+        trailTextures.put("donator_trail", (Object) null);
+        trailTextures.put("helper_trail", (Object) null);
+    }
+}
